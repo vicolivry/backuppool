3564b7e800dbb5253546ddb3675ff81dfdb7b33f
Victor Olivry <volivry@z2r1p3.le-101.fr>
2017-09-18 15:31:38 +0200

    deuxieme commit, fonction presque finie

---
+++ gitbackhub/Rush01/Lib/ft_check_if_safe.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_if_safe.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: volivry <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/18 11:41:33 by volivry           #+#    #+#             */
+/*   Updated: 2017/09/18 15:22:23 by volivry          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_safe_line_col(int grid[9][9], int x, int y, int nb)
+{
+	int cursor;
+
+	cursor = 0;
+	while (cursor <= 9)
+	{
+		if (grid[x][cursor] == nb)
+			return (0);
+		cursor++;
+	}
+	cursor = 0;
+	while (cursor <= 9)
+	{
+		if (grid[cursor][y] == nb)
+			return (0);
+		cursor++;
+	}
+	return (1);
+}
+
+int	ft_safe_small_grid(int grid[9][9], int x, int y, int nb)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (i <= 2)
+	{
+		while (j <= 2)
+		{
+			if (grid[x + i][y + j] == nb)
+				return (0);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (1);
+}
+
+int ft_safe_nb(int grid[9][9],int x, int y, int nb)
+{
+	if (ft_safe_line_col(grid, x, y, nb) == 1
+			&& ft_safe_small_grid(grid, x - x % 3, y - y % 3, nb) == 1)
+		return (1);
+	return (0);
+}
---
+++ gitbackhub/Rush01/Lib/main.c
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_display_grid.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: volivry <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/17 14:56:20 by volivry           #+#    #+#             */
+/*   Updated: 2017/09/18 15:29:58 by volivry          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_checkchar(char *str, int argc);
+
+void	ft_putchar(char c);
+
+int		ft_atoi_modified(char *str);
+
+void	ft_putstr(char *str);
+
+int		ft_strlen(char *str);
+
+void	ft_special_putstr(int *tab);
+
+int ft_safe_nb(int grid[9][9], int x, int y, int nb);
+
+void	ft_fill_grid(int grid[9][9], char **argv)
+{
+	int	line;
+	int	col;
+
+	line = 1;
+	col = 0;
+	while (line < 10)
+	{
+		while (col <= 9)
+		{
+			if (argv[line][col] == '.')
+				grid[line - 1][col] = 0;
+			else if (argv[line][col] >= '1' && argv[line][col] <= '9')
+				grid[line - 1][col] = argv[line][col] - '0';
+			col++;
+		}
+		col = 0;
+		line++;
+	}
+}
+
+int		ft_find_unassigned(int grid[9][9], int *l, int *c)
+{
+	while (*l < 9)
+	{
+		while (*c < 9)
+		{
+			if (grid[*l][*c] == 0)
+				return (1);
+			*c = *c + 1;
+		}
+		*c = 0;
+		*l = *l + 1;
+	}
+	return (0);
+}
+
+int		ft_solve_sudoku(int grid[9][9])
+{
+	int line;
+	int col;
+	int number;
+
+	line = 0;
+	col = 0;
+	if (ft_find_unassigned(grid, &line, &col) == 0)
+		return (1);
+	number = 1;
+	while (number <= 9)
+	{
+		if (ft_safe_nb(grid, line, col, number) == 1)
+			grid[line][col] = number;
+		if (ft_solve_sudoku(grid) == 1)
+			return (1);
+	}
+	return (0);
+}
+
+int		main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+	int grid[9][9];
+
+	i = 0;
+	j = 0;
+	while (j < argc - 1)
+	{
+				if (ft_checkchar(argv[j + 1], argc) == 0)
+		{
+			ft_putstr("Error\n");
+			return (0);
+		}
+		j++;
+	}
+	ft_fill_grid(grid, argv);
+//	if (ft_solve_sudoku(grid) == 0)
+//	{
+//		ft_putstr("Error\n");
+//		return (0);
+//	}
+	while (i < argc - 1)
+	{
+		ft_special_putstr(grid[i]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
+
